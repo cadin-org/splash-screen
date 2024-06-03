6f9e28d8b63bae7b8e7e9607b53b523cb3536cb6
lpnh <paniguel.lpnh@gmail.com>
2024-06-03 18:19:23 -0300

    add minimal implementation

---
+++ main.lua
@@ -0,0 +1,5 @@
+function love.draw()
+  local fira_mono = love.graphics.newFont('assets/fonts/FiraMono-Medium.ttf', 48)
+  love.graphics.setFont(fira_mono)
+  love.graphics.print('cadin + LÖVE', 400, 300)
+end
