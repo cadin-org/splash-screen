de26259b8a0cab718f83457106fb3dbf3771f6a3
Vinicius Tibo <viniciustibo.vt@gmail.com>
2024-06-16 20:42:28 -0300

    refactor: M.start

--- init.lua
+++ init.lua
@@ -1,27 +1,26 @@
 local M = {}
 
-M.canvas = nil
-M.x = nil
-M.y = nil
+local canvas
+local x, y
 
 local function text_center(text)
   local current_font = love.graphics.getFont()
   local text_height = current_font:getHeight()
   local text_width = current_font:getWidth(text)
 
-  local x = (love.graphics.getWidth() - text_width) / 2
-  local y = (love.graphics.getHeight() - text_height) / 2
+  local x_text = (love.graphics.getWidth() - text_width) / 2
+  local y_text = (love.graphics.getHeight() - text_height) / 2
 
-  return x, y, text_width
+  return x_text, y_text, text_width
 end
 
 local function splash_colored_text(r, g, b, text)
   local red, green, blue = love.math.colorFromBytes(r, g, b)
-  local x, y, text_width = text_center(text)
+  local x_text, y_text, text_width = text_center(text)
 
   local colored_text = { { red, green, blue }, text }
 
-  love.graphics.printf(colored_text, x, y, text_width, 'center')
+  love.graphics.printf(colored_text, x_text, y_text, text_width, 'center')
   -- TODO: Add fade in/fade out effect
 end
 
@@ -29,27 +28,23 @@ function M.new(width, height)
   local x_center = love.graphics.getWidth() / 2
   local y_center = love.graphics.getHeight() / 2
 
-  M.x = x_center - (width / 2)
-  M.y = y_center - (height / 2)
+  x = x_center - (width / 2)
+  y = y_center - (height / 2)
 
-  M.canvas = love.graphics.newCanvas(width, height)
-  love.graphics.setCanvas(M.canvas)
+  canvas = love.graphics.newCanvas(width, height)
+  love.graphics.setCanvas(canvas)
   love.graphics.clear(love.math.colorFromBytes(30, 30, 46))
   love.graphics.setCanvas()
 end
 
 function M.start(time)
-  love.graphics.draw(M.canvas, M.x, M.y)
+  love.graphics.draw(canvas, x, y)
 
-  if time >= 3 then
-    love.graphics.draw(M.canvas, M.x, M.y)
+  if time >= 3 and time < 6 then
     splash_colored_text(243, 139, 168, 'LÖVE')
-  end
-  if time >= 6 then
-    love.graphics.draw(M.canvas, M.x, M.y)
+  elseif time >= 6 and time < 9 then
     splash_colored_text(205, 214, 244, 'cadin')
-  end
-  if time >= 9 then
+  elseif time >= 9 then
     love.graphics.clear()
   end
 end
